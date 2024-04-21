eaa6f27abb12c77f01e23880443789fd4e0b5be8
martijnvancitteren <martijnvancitteren@live.nl>
2024-04-21 21:04:40 +0200

    happy flow user controller in place

--- src/test/java/com/martijn/CompCheckV2/rest/UserControllerTest.java
+++ src/test/java/com/martijn/CompCheckV2/rest/UserControllerTest.java
@@ -1,24 +1,33 @@
 package com.martijn.CompCheckV2.rest;
 
-import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.martijn.CompCheckV2.rest.dto.UserDto;
+import com.martijn.CompCheckV2.rest.dto.UserDtoFactory;
 import com.martijn.CompCheckV2.rest.dto.requests.UserRegisterRequest;
 import com.martijn.CompCheckV2.rest.dto.requests.UserRegistrationRequestFactory;
 import com.martijn.CompCheckV2.rest.dto.response.UserRegisterResponse;
+import com.martijn.CompCheckV2.rest.dto.response.UserRegistrationResponseFactory;
+import com.martijn.CompCheckV2.rest.mapper.UserMapper;
 import com.martijn.CompCheckV2.service.UserService;
-import jdk.jfr.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultMatcher;
 
-import static org.springframework.http.RequestEntity.post;
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @WebMvcTest
 class UserControllerTest {
@@ -29,26 +38,39 @@ class UserControllerTest {
     @MockBean
     UserService userService;
 
-    private final String registerUri = "api/compcheck/user/register";
-    private ObjectMapper objectMapper;
+    private final String registerUri = "/api/compcheck/user/register";
+    private ObjectWriter jsonWriter;
     private UserRegisterRequest registerRequest;
+    private UserDto userDto;
+
+
 
     @BeforeEach
     void setUp() {
-        objectMapper = new ObjectMapper(new JsonFactory());
-
+        jsonWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        userDto = UserDtoFactory.createUserDto().build();
     }
 
     @Test
     void givenValidRegistrationRequest_whenRegisteringNewUser_thenShouldReturnUserAndStatus201() throws Exception {
         //given
         registerRequest = UserRegistrationRequestFactory.userRegisterRequest().build();
+        UserRegisterResponse expectedResponse = UserRegistrationResponseFactory.userRegisterResponse().build();
 
-        //when & then
+        //when
+        when(userService.registerUser(UserMapper.requestToDto(registerRequest))).thenReturn(userDto);
+
+        //then
         mockMvc.perform(post(registerUri)
-                .accept(MediaType.APPLICATION_JSON));
-
-
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonWriter.writeValueAsString(registerRequest)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName").value(expectedResponse.firstName()))
+                .andExpect(jsonPath("$.lastName").value(expectedResponse.lastName()))
+                .andExpect(jsonPath("$.email").value(expectedResponse.email()))
+                .andExpect(jsonPath("$.city").value(expectedResponse.city()))
+                .andExpect(jsonPath("$.country").value(expectedResponse.country()));
     }
 
 }
