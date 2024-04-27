8856f80dfce16eef2a6db80b3f7fd9bf8fd696f9
martijnvancitteren <martijnvancitteren@live.nl>
2024-04-27 19:00:32 +0200

    start implementing JWT

--- src/main/java/com/martijn/CompCheckV2/CompCheckV2Application.java
+++ src/main/java/com/martijn/CompCheckV2/CompCheckV2Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CompCheckV2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(CompCheckV2Application.class, args);
     }
