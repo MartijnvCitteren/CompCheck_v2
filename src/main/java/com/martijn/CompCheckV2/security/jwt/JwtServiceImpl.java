c6f1de7176eba1e8bda72b2977a3694f36d9de93
martijnvancitteren <martijnvancitteren@live.nl>
2024-05-04 15:15:08 +0200

    decode methods implementing

--- src/main/java/com/martijn/CompCheckV2/security/jwt/JwtServiceImpl.java
+++ src/main/java/com/martijn/CompCheckV2/security/jwt/JwtServiceImpl.java
@@ -1,5 +1,12 @@
 package com.martijn.CompCheckV2.security.jwt;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.JwtParserBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -10,21 +17,31 @@ import java.util.Date;
 
 @Component
 public class JwtServiceImpl implements jwtService {
+    private final String SECRET_KEY = "normally_this_secret_key_will_be_hidden_somewhere_else_but_for_for_now_this_is_okay";
 
     @Override
     public String generateToken(String email) {
-        final long experationTimeInSeconds = 30 * 24 * 60 * 60 * 60;
+        final long expirationTimeInSeconds = 30 * 24 * 60 * 60 * 60;
         return Jwts.builder()
                 .issuer("CompCheck")
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + experationTimeInSeconds))
+                .expiration(new Date(System.currentTimeMillis() + expirationTimeInSeconds))
                 .subject(email)
-                .signWith(createSecretKey())
+                .signWith(creatSecretKey())
                 .compact();
     }
 
-    private Key createSecretKey() {
-        byte[] keyBytes = Decoders.BASE64.decode("normally_this_secret_key_will_be_hidden_somewhere_else_but_for_for_now_this_is_okay");
+    @Override
+    public boolean tokenIsValid(String token) {
+        try{
+            DecodedJWT decodedJWT = verifier.verify(token);
+        }
+
+        return true;
+    }
+
+    private Key creatSecretKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
--- src/main/java/com/martijn/CompCheckV2/security/jwt/jwtService.java
+++ src/main/java/com/martijn/CompCheckV2/security/jwt/jwtService.java
@@ -2,4 +2,5 @@ package com.martijn.CompCheckV2.security.jwt;
 
 public interface jwtService {
     String generateToken(String email);
+    boolean tokenIsValid(String token);
 }
